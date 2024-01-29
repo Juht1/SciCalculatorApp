47c8a78efe72f736724c23a3261a300c0e9544c4
Opilane TTHK <opilane@tthk.ee>
2024-01-29 12:23:56 +0200

    Globalusing
    Added GlobalUsing And mainPage

--- SciCalculatorApp/SciCalculatorApp/App.xaml.cs
+++ SciCalculatorApp/SciCalculatorApp/App.xaml.cs
@@ -19,11 +19,13 @@ public partial class App : Application
 		var mauiWindow = handler.VirtualView;
 		var nativeWindow = handler.PlatformView;
 		nativeWindow.Activate();
-		IntPtr hWindowHandle = WinRT.Interop.WindowNative.GetWindowHandle(nativeWindow);
-		WindowId windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(windowhandle);
+		IntPtr windowHandle = WinRT.Interop.WindowNative.GetWindowHandle(nativeWindow);
+		WindowId windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(windowHandle);
 		AppWindow appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
 		appWindow.Resize(new SizeInt32(WindowWidth, WindowHeight));
 	});
 #endif
+
+		MainPage = new CalculatorPage();
 	}
 }
