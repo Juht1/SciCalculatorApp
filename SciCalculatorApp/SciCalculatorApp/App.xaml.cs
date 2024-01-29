916735e14d6b268b10502be3618d2445dbd74880
Opilane TTHK <opilane@tthk.ee>
2024-01-29 12:14:05 +0200

    TeineCommit
    Tegime ViewFolderi ja removisin 2-te folderit

--- SciCalculatorApp/SciCalculatorApp/App.xaml.cs
+++ SciCalculatorApp/SciCalculatorApp/App.xaml.cs
@@ -1,11 +1,29 @@
 ﻿namespace SciCalculatorApp;
+#if WINDOWS
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using Windows.Graphics;
+#endif
 
 public partial class App : Application
 {
+	const int WindowWidth = 540;
+	const int WindowHeight = 1000;
+
 	public App()
 	{
 		InitializeComponent();
-
-		MainPage = new AppShell();
+#if WINDOWS
+	Microsoft.Maui.Handlers.WindowHandler.Mapper.AppendToMapping(nameof(IWindow), (handler, view) =>
+	{
+		var mauiWindow = handler.VirtualView;
+		var nativeWindow = handler.PlatformView;
+		nativeWindow.Activate();
+		IntPtr hWindowHandle = WinRT.Interop.WindowNative.GetWindowHandle(nativeWindow);
+		WindowId windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(windowhandle);
+		AppWindow appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
+		appWindow.Resize(new SizeInt32(WindowWidth, WindowHeight));
+	});
+#endif
 	}
 }
